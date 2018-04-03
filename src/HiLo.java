3508b4911b9b14da52d9a61c0570e4fddb290e18
Kelly <k.tompkins005@gmail.com>
2018-04-03 09:45:32 -0500

    Initial commit

---
+++ src/HiLo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class HiLo {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		// randomly generated number
+		int theNumber = (int)(Math.random() * 100 + 1);
+		System.out.println(theNumber);
+		int guess = 0;
+		while (guess != theNumber) {
+		System.out.println("Guess a number between 1 and 100");
+		guess = scan.nextInt();
+		if ( guess < theNumber )
+			System.out.println(guess + " is too low. Try again.");
+		else if (guess > theNumber )
+			System.out.println(guess + " is too high. Try again.");
+		else
+			System.out.println(guess + " is correct. You win!");
+		}
+	}
+
+}
